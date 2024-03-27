355c2ffe07233b7f5596a4b05725d383258ecce9
SonK4m3 <nguyenhongsont1lhp@gmail.com>
2024-03-27 14:38:14 +0700

    refactor: remove book, product model

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,13 @@
 import { FastifyInstance, FastifyRequest } from 'fastify';
 import userRoutes from './user.route';
-import productRoutes from './product.route';
-import bookRoutes from './book.route';
 
 import { userSchemas } from '../validators/user.schema';
-import { productSchemas } from '../validators/product.schema';
-import { bookSchemas } from '../validators/book.schema';
 import fooRoutes from './foo.route';
 
 type Role = 'admin' | 'user';
 
 const routes = async (server: FastifyInstance) => {
-  for (const schema of [...userSchemas, ...productSchemas, ...bookSchemas]) {
+  for (const schema of [...userSchemas]) {
     server.addSchema(schema);
   }
 
@@ -35,8 +31,6 @@ const routes = async (server: FastifyInstance) => {
   // register sub-routes
   server.register(fooRoutes, { prefix: '/foo' });
   server.register(userRoutes, { prefix: '/users' });
-  server.register(productRoutes, { prefix: '/products' });
-  server.register(bookRoutes, { prefix: '/books' });
 };
 
 export default routes;
