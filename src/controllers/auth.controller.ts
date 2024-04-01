72229a911fcce7acb5ad48329d7bd93499d2284a
SonK4m3 <nguyenhongsont1lhp@gmail.com>
2024-04-01 16:49:51 +0700

    refactor: add reply code, message for login, signup usecase

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -17,14 +17,26 @@ class UserController {
     return reply.code(201).send(user);
   }
 
+  async signupHandler(request: FastifyRequest<{ Body: CreateUserInput }>, reply: FastifyReply) {
+    const body = request.body;
+
+    const user = await userService.createUser(body);
+
+    const { password, salt, ...rest } = user;
+    const token = await reply.jwtSign(rest);
+    return {
+      accessToken: token,
+    };
+  }
+
   async loginHandler(request: FastifyRequest<{ Body: LoginInput }>, reply: FastifyReply) {
     const body = request.body;
 
     const user = await userService.findUserByEmail(body.email);
-
     if (!user) {
       return reply.code(401).send({
-        message: 'Invalid email or password',
+        code: 'NOT_FOUND',
+        message: 'Email does not register!',
       });
     }
 
@@ -41,6 +53,11 @@ class UserController {
         accessToken: token,
       };
     }
+
+    return reply.code(401).send({
+      code: 'INVALID',
+      message: 'Invalid password',
+    });
   }
 
   async getUsersHandler() {
