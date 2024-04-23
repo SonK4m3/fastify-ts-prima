c82737b142d796ace6332f985fb00c9597d52619
SonK4m3 <nguyenhongsont1lhp@gmail.com>
2024-04-23 15:43:37 +0700

    feat: create permission decorater to check permission of each role

--- src/plugins/authorization.plugin.ts
+++ src/plugins/authorization.plugin.ts
@@ -1,6 +1,8 @@
 import fastifyPlugin from 'fastify-plugin';
 import fastifyAuth, { FastifyAuthPluginOptions } from '@fastify/auth';
 import { FastifyRequest, FastifyReply, FastifyInstance, errorCodes } from 'fastify';
+import { roles } from '../models/role';
+import PermissionError from '../models/error/permission.error';
 
 export default fastifyPlugin<FastifyAuthPluginOptions>(async (fastify: FastifyInstance) => {
   fastify.register(fastifyAuth);
@@ -13,6 +15,18 @@ export default fastifyPlugin<FastifyAuthPluginOptions>(async (fastify: FastifyIn
       const userRoles: string[] = request.user ? [request.user.role] : [];
       if (!allowedRoles?.some((role: string) => userRoles.includes(role))) {
         throw new errorCodes.FST_ERR_REP_INVALID_PAYLOAD_TYPE();
+      }
+    }
+  });
+
+  fastify.decorate('permission', async (request: FastifyRequest, reply: FastifyReply) => {
+    // declare type in src/index.ts line 24
+    const permission = request.routeOptions.config.permission;
+    const userRole = request.user.role;
+
+    if (permission && userRole) {
+      if (!roles[userRole].can.includes(permission)) {
+        throw new PermissionError(`The ${userRole} does not have ${permission} permission!`);
       }
     }
   });
