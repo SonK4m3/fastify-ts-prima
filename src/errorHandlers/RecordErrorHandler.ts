3fc87693fc8c1f3885d24e0324de2d961c74d7d4
SonK4m3 <nguyenhongsont1lhp@gmail.com>
2024-04-25 15:42:47 +0700

    feat: create class error handler

--- src/errorHandlers/RecordErrorHandler.ts
+++ src/errorHandlers/RecordErrorHandler.ts
@@ -1,5 +1,6 @@
 import { errorCodes, FastifyError, FastifyReply, FastifyRequest } from 'fastify';
 import { PermissionError } from '../models/error';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 export class RecordErrorHandler {
   static handle(error: FastifyError, request: FastifyRequest, reply: FastifyReply) {
@@ -7,6 +8,10 @@ export class RecordErrorHandler {
       reply.status(500).send({ message: 'Server internal error!' });
     } else if (error instanceof PermissionError) {
       reply.status(403).send({ message: error.message });
+    } else if (error instanceof PrismaClientKnownRequestError) {
+      if (error.code === 'P2002') {
+        reply.status(500).send({ code: error.code, message: error.message });
+      }
     } else {
       reply.send(error);
     }
